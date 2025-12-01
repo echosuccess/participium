3e5535bf0ebb7ad2b987047495741027c3d6972a
gabbo0501 <gabrielemondino05@gmail.com>
2025-12-01 20:27:27 +0100

    Rimuovi dist dal versionamento

--- server/dist/server/src/routes/rootRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const errorMiddleware_1 = require("../middlewares/errorMiddleware");
-const rootController_1 = require("../controllers/rootController");
-const router = (0, express_1.Router)();
-router.get("/", (0, errorMiddleware_1.asyncHandler)(rootController_1.getApiInfo));
-exports.default = router;
--- server/dist/src/utils/prismaClient.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const GeneratedPrisma = require("../../generated/prisma");
-const { PrismaClient } = GeneratedPrisma;
-const prisma = new PrismaClient();
-exports.default = prisma;
